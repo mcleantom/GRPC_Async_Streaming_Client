2202e67e1516836b2874beea0ac0f7c4d8d66530
mcleantom <mcleantom97@gmail.com>
2022-12-20 11:36:58 +0000

    proto files building and library links to Client

---
+++ client/client.cpp
@@ -0,0 +1,7 @@
+#include "client.h"
+#include <iostream>
+#include <ProtoObjects/helloworld.pb.h>
+
+int main() {
+    std::cout << "Hello!" << std::endl;
+}
---
+++ client/client.h
@@ -0,0 +1,7 @@
+#ifndef GRPC_ASYNC_STREAMING_CLIENT_H
+#define GRPC_ASYNC_STREAMING_CLIENT_H
+
+#include <iostream>
+
+
+#endif
